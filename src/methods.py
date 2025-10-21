1a5353dc05fee0713e761f3b7458f09c7454d094
Frikk Brændsrød <fribra05@yahoo.com>
2025-10-21 23:03:51 +0200

    Reverted back to pre +rep

--- src/methods.py
+++ src/methods.py
@@ -17,28 +17,37 @@ intents.message_content = True
 client = discord.Client(intents=intents)
 
 def help():
-    helpText = ("Use key as keyword to get started!   eks: key ept\n    ept: Shows time until the EPT CTF\n    base64: Use base64 xxxx== to decode base64 directly\n   sem_goon: Shows the sem goon pipeline\n  huzz: Shows the huzz\n \n   update/test bot pulls most recent version in either main or testing branch")
+    #validateCmd(message)
+    helpText = ("Use key as keyword to get started!   eks: key ept\n\n\nept: Shows time until the EPT CTF\n\nbase64: Use base64 xxxx== to decode base64 directly\n \nsem_goon: Shows the sem goon pipeline\n\nhuzz: Shows the huzz\n\n\nupdate/test bot pulls most recent version in either main or testing branch")
 
     return helpText
 
+def validateCmd(message):
+    emoji = discord.utils.get(message.guild.emojis, name="plussrep")
+    message.add_reaction(emoji)
 
-def biletter():
+def biletter(link):
+    #validateCmd(message)
     if b"Tilgjengelige varer" in urllib.request.urlopen(link).read():
         return(f"Tickets available at {link}")
     else:
         return ("No tickets yet!")
 
 def updateBot():
+    #validateCmd(message)
     subprocess.run(["./roll-out.sh"])
 
 def testBot():
+    #validateCmd(message)
     subprocess.run(["./testing.sh"])
 
 def base64(content):
+    #validateCmd(message)
     message_bytes = base64.b64decode(content)
     return (f"Decoded base64: {message_bytes.decode('utf-8', errors='ignore')}")
 
 def ept():
+    #validateCmd(message)
     event_date = td.datetime(2025, 11, 8, 10)
     now = td.datetime.now()
     delta = event_date - now
@@ -52,3 +61,4 @@ def eptShort():
     now = td.datetime.now()
     delta = event_date - now
     days, seconds = delta.days, delta.seconds
+    return (f"EPT in: {days} days")
