6e06e1445d004d97ed051a6bebea062337da7816
Frikk Brændsrød <fribra05@yahoo.com>
2025-10-31 13:10:07 +0100

    Added Arpit to repBoard

--- src/methods.py
+++ src/methods.py
@@ -121,7 +121,7 @@ def pRep(message, user):
 def mRep(message, user):
     if user == message.author:
         return "❌ You cannot give reputation to yourself!"
-    elif user in ["Simen", "Frikk", "Mathias", "Odin", "Joakim", "Nick"]:
+    elif user in ["Simen", "Frikk", "Mathias", "Odin", "Joakim", "Nick", "Arpit"]:
 
         emoji = discord.utils.get(message.guild.emojis, name="plusrep")
         if emoji is None:
@@ -137,7 +137,7 @@ def mRep(message, user):
 
         return f"⚠️ Gave -1 rep to **{user_key}**! Total: **{reps[user_key]}** {emoji}"
     else:
-        return "❌ You can only give reputation to Simen, Frikk, Mathias, Odin, Joakim, or Nick!"
+        return "❌ You can only give reputation to Simen, Frikk, Mathias, Odin, Joakim, Arpit, or Nick!"
 
 
 
